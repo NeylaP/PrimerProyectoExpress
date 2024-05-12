1a27708ce59558de77bd2b50f4f49f5a62edb987
NeylaP <neyla1998@outlook.es>
2024-05-11 19:42:03 -0500

    feat: Crud users without logic

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.post('/api/users', (request, response)=>{
+    response.status(200).send('Create user');
+});
+
+app.get('/api/users', (request, response)=>{
+    response.status(200).send('Get user');
+});
+app.put('/api/users', (request, response)=>{
+    response.status(500).send('Update user');
+});
+app.delete('/api/users', (request, response)=>{
+    response.status(400).send('Delete user');
+});
+
 app.listen(PORT, 
     ()=> {
         console.log(`Server is running Neyla`);
