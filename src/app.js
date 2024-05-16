dd3dd1ca6d04e4abe1ccfc2e0d3e18173fa9d4f1
NeylaP <neyla1998@outlook.es>
2024-05-15 20:44:11 -0500

    feat: Se creo modelo

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 4000;
 
-app.post('/api/users', userController.createUser);
-
-app.get('/api/users', userController.getUser);
-
-app.put('/api/users', userController.updateUser);
-
-app.delete('/api/users/:id', userController.deleteUser);
+app.post('/api/users', userController.createUser)
+// recuperar todos los usuarios
+app.get('/api/users', userController.getUsers)
+// actulizar un user
+app.put('/api/users', userController.updateUser)
+// eliminar user
+app.delete('/api/users/:id', userController.deleteUser)
+// recuperar usuario por id
+app.get('/api/users/:id', userController.findUserById)
 
 app.listen(PORT, 
     ()=> {
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,21 +1,79 @@
+// const db = require("../database/db");
+
+const User = require("../database/models/User");
+
 const userController = {};
 
-userController.createUser = (request, response)=>{
-    response.status(200).send('Create user');
+userController.createUser = (request, response) => {
+	// recuperar la info a guardar en bd
+	const name = request.body.name;
+	const email = request.body.email;
+	const password = request.body.password;
+	const isActive = 1;
+	const roleId = 1;
+
+	// Validaciones
+	if (!name || !email || !password) {
+		return response.status(400).json("Name, email and password are required");
+	}
+
+	// tratar la informacion como por ejemplo encriptar la contraseña
+
+	// guardar en bd
+	User.create({
+		name: name,
+		email: email,
+		password: password,
+		isActive: isActive,
+		roleId: roleId,
+	});
+
+	// finalmente siempre mi api debe devolver una respuesta
+	response.status(201).json("CREATE USER");
 };
 
-userController.getUser = (request, response)=>{
-    response.status(200).send('Get user');
+userController.getUsers = (request, response) => {
+	// consultar en la base de datos
+	const users = User.find();
+
+	response.status(200).json(users);
 };
 
-userController.updateUser = (request, response)=>{
-    console.log(request.body);
-    response.status(200).send('Update user');
+userController.updateUser = (request, response) => {
+	console.log("-------------------");
+	console.log(request.body);
+	console.log("-------------------");
+
+	response.status(200).send("UPDATE USER");
 };
 
-userController.deleteUser = (request, response)=>{
-    response.status(400).send(`Delete user ${request.params.id}`);
+userController.deleteUser = (request, response) => {
+	// recuperar el id del usuario a eliminar
+	const userIdToDelete = request.params.id;
+
+	// eliminar de la db el usuario por id
+	const userDeleted = User.delete(userIdToDelete);
+
+	if (userDeleted) {
+		response.status(200).json("DELETE USER " + request.params.id);
+	} else {
+		response.status(404).json("User doesnt exist: " + request.params.id);
+	}
 };
 
+userController.findUserById = (request, response) => {
+	// recuperar el id del usuario que quieto recupear
+	const userId = request.params.id;
+
+	// recuperar el usuario de la base de datos a traves del modelo
+	const user = User.findById(userId);
+
+	// finalmante devolvemos una respuesta
+	if (!user) {
+		response.status(404).json("User doesnt exists");
+	} else {
+		response.status(200).json(user);
+	}
+};
 
 module.exports = userController;
